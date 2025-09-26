e80beb49d2b74489f13694ce89a2f0e4000a2066
Ahmed9190 <ahmed.waleed919@outlook.com>
2025-09-26 19:44:42 +0300

    Updated build configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/nisa',
   integrations: [tailwind()],
   output: 'static',
   vite: {
