38a2afbc8f0a9fd48b0d884bbb9d66544386af96
Ahmed9190 <ahmed.waleed919@outlook.com>
2026-06-23 20:49:28 +0300

    Updated products

--- scratch-test.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-const fileContent = fs.readFileSync('c:/Users/HP/Desktop/nisa-website/src/data/products.json', 'utf8');
-const data = JSON.parse(fileContent);
-const products = data.products || [];
-
-const velora = products.find(p => p.id === 'summer-velora');
-console.log(JSON.stringify(velora, null, 2));
